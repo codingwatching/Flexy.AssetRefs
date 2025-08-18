9566cb6b05197e083fac3e9fa382686f63600fc0
JesOb <JesObertyukh@gmail.com>
2025-08-18 13:37:32 +0200

    Changed better editor menu naming

--- Runtime/AssetsLoader.cs
+++ Runtime/AssetsLoader.cs
@@ -14,8 +14,8 @@ public abstract class AssetsLoader
 	private static	Boolean?		_runtimeBehaviorEnabled;
 	public static	Boolean			RuntimeBehaviorEnabled		
 	{
-		get => _runtimeBehaviorEnabled ??= UnityEditor.EditorPrefs.GetBool( Application.productName + "=>Flexy/AssetRefs/RuntimeBehaviorEnabled" ); 
-		set => UnityEditor.EditorPrefs.SetBool( Application.productName + "=>Flexy/AssetRefs/RuntimeBehaviorEnabled", (_runtimeBehaviorEnabled = value).Value );
+		get => _runtimeBehaviorEnabled ??= UnityEditor.EditorPrefs.GetBool( Application.productName + "=>Fun.Flexy/AssetRefs/RuntimeBehaviorEnabled" ); 
+		set => UnityEditor.EditorPrefs.SetBool( Application.productName + "=>Fun.Flexy/AssetRefs/RuntimeBehaviorEnabled", (_runtimeBehaviorEnabled = value).Value );
 	}
 #endif
 	
@@ -299,7 +299,7 @@ public abstract class AssetsLoader
 	public static class Editor
 	{
 #if UNITY_EDITOR
-		public const String Menu = "Tools/Flexy/AssetRefs/AssetLoader/";
+		public const String Menu = "Tools/Fun.Flexy/Asset Refs/Asset Loader/";
 		[UnityEditor.MenuItem( Menu+"Enable Runtime Behavior", secondaryPriority = 101)]	static void		EnableRuntimeBehavior			( ) => RuntimeBehaviorEnabled = true;
 		[UnityEditor.MenuItem( Menu+"Disable Runtime Behavior", secondaryPriority = 100)]	static void		DisableRuntimeBehavior			( ) => RuntimeBehaviorEnabled = false;
 		[UnityEditor.MenuItem( Menu+"Enable Runtime Behavior", true)]						static Boolean	EnableRuntimeBehaviorValidate	( ) => !RuntimeBehaviorEnabled;
